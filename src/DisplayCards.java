2875320ec0334e244ef66c3074b8d6de42afc49a
Azeem-Hussaini <syed.hussaini3@ontariotechu.net>
2020-03-05 04:00:05 -0500

    Completed

---
+++ src/DisplayCards.java
@@ -0,0 +1,38 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class DisplayCards extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane = new HBox(10);
+        pane.setPadding(new Insets(5, 5, 5, 5));
+
+        int random = 0;
+        String string1 = "";
+        String card = "";
+        for (int i = 0; i < 3; i++) {
+            random = (int) (Math.random() * 64 + 1);
+            string1 = Integer.toString(random);
+            card = "/Cards/" + string1 + ".png";
+            Image image = new Image(card);
+            ImageView imageView1 = new ImageView(image);
+            pane.getChildren().add(imageView1);
+        }
+
+
+
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DisplayCards");
+        primaryStage.show();
+
+
+
+    }
+}
